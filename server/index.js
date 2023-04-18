71e8fcd2ba5a8ce8f59a289096934f4a34d68b2c
valentine michael <michael.chionye@gmail.com>
2023-04-18 10:01:42 +0100

    Set up react and express server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.set(cors());
+app.set(express.json());
+app.set(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("this is a test");
+});
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
